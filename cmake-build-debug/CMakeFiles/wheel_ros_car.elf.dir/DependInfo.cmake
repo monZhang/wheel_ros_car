0238d04bc528305abbce6328d91b36cdb0a51be5
cxzhanglong <cxzhanglong@didiglobal.com>
2023-07-18 17:31:25 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/DependInfo.cmake
@@ -0,0 +1,89 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/didi/CLionProjects/wheel_ros_car/startup/startup_stm32f407xx.s" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../CORE/Inc"
+  "../SYSTEM/Inc"
+  "../FWLIB/Inc"
+  "../USER/Inc"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/didi/CLionProjects/wheel_ros_car/CORE/Src/stm32f4xx_it.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/CORE/Src/stm32f4xx_it.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/CORE/Src/system_stm32f4xx.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/CORE/Src/system_stm32f4xx.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/misc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/misc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_adc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_adc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_can.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_can.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_crc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_crc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_cryp.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_cryp.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_cryp_aes.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_cryp_aes.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_cryp_des.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_cryp_des.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_cryp_tdes.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_cryp_tdes.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_dac.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_dac.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_dbgmcu.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_dbgmcu.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_dcmi.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_dcmi.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_dma.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_dma.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_dma2d.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_dma2d.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_exti.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_exti.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_flash.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_flash.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_flash_ramfunc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_flash_ramfunc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_fsmc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_fsmc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_gpio.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_gpio.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_hash.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_hash.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_hash_md5.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_hash_md5.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_hash_sha1.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_hash_sha1.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_i2c.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_i2c.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_iwdg.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_iwdg.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_ltdc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_ltdc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_pwr.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_pwr.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_rcc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_rcc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_rng.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_rng.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_rtc.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_rtc.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_sai.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_sai.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_sdio.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_sdio.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_spi.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_spi.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_syscfg.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_syscfg.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_tim.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_tim.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_usart.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_usart.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/FWLIB/src/stm32f4xx_wwdg.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/FWLIB/src/stm32f4xx_wwdg.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/SYSTEM/Src/delay.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/SYSTEM/Src/delay.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/SYSTEM/Src/sys.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/SYSTEM/Src/sys.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/SYSTEM/Src/usart.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/SYSTEM/Src/usart.c.obj"
+  "/Users/didi/CLionProjects/wheel_ros_car/USER/Src/main.c" "/Users/didi/CLionProjects/wheel_ros_car/cmake-build-debug/CMakeFiles/wheel_ros_car.elf.dir/USER/Src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CORE/Inc"
+  "../SYSTEM/Inc"
+  "../FWLIB/Inc"
+  "../USER/Inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
